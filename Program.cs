dd850da3b43dcaa15ae971f7b186e6a1b3dd2518
jenis <jenis@MATHUKIYAJENISH>
2025-03-19 19:09:26 +1300

    chek

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ class program
     static void main(String[] args)
     {
         Console.WriteLine("hello jenish");
-        Console.WriteLine("how are you"); 
+        Console.WriteLine("how are you");
+        Console.WriteLine("hey");
             }
 }
